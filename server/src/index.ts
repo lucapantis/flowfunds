83b9ff5b5b8c46565f86e6bc93fb8269dfe51797
Vlad <lucapantis@gmail.com>
2026-07-11 15:25:56 +0300

    Set up Express backend

---
+++ server/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();  // creates the backend app
+const PORT = 3001;
+
+app.use(cors());  // allows front and back communication
+app.use(express.json());  // allows backend to read json request bodies later
+
+// // simple test route to prove server works
+app.get("/api/health", (req, res) => {
+    res.json({
+        status: "ok",
+        message: "FlowFunds API is running",
+    })
+})
+
+// // starts backend on port 3001
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
